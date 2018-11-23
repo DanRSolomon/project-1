31e1487f756bf3d9c49c182f4b248d8f40d0ffb0
Hail Mary <41487770+alejosjen@users.noreply.github.com>
2018-11-23 07:57:03 -0700

    start of oxford dictionary api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function () {
   }
   //Clear function
   clear();
-  
+
   //Hide compilation search areas
   $(".card-deck").toggle(false);
   //Search compilations button with onClick function
@@ -87,57 +87,56 @@ document.addEventListener('DOMContentLoaded', function () {
       };
     });
 
-  //Toggle off compilation card-deck to keep the appearance clean
-  $(".card-deck").toggle(false);
-  //Search compilations button with onClick function
-  $(document).on("click", '.compilation-search-button', function () {
-    clear();
-    //Toggle on card-deck after onClick function
-    $(".card-deck").toggle();
 
-    //Robohash.org
-    var searchTerm = $("#compilation-input").val();
-    var url = "https://robohash.org/" + searchTerm + ".png";
-    var robotResult = $(`
-        <img src="${url}" class="img-fluid robot-result" alt="Robot Image">
-        <h3 class="card-title">Your Robot</h3>
-        <p>Robots lovingly delivered by <a href="https://robohash.org/">Robohash.org</a></p>
-    `)
-    robotResult.appendTo(".robot-result");
-    //end of Robohash.org
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyDahpCBh75i8lWv0Rb-m2EK9t2nvyC2rik",
+      authDomain: "this-is-a-test-bb2be.firebaseapp.com",
+      databaseURL: "https://this-is-a-test-bb2be.firebaseio.com",
+      projectId: "this-is-a-test-bb2be",
+      storageBucket: "this-is-a-test-bb2be.appspot.com",
+      messagingSenderId: "890505952792"
+    };
+    firebase.initializeApp(config);
 
-    //OpenLibrary.org
-    var userSubjectSearch = $("#compilation-input").val();
-    var uriSubjectSearch = encodeURI(userSubjectSearch);
-    var queryURL = "https://openlibrary.org/subjects/" + uriSubjectSearch + ".json";
+    var database = firebase.database();
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function (response) {
-      console.log(response);
-      var bookWorks = response.works;
-      if (bookWorks.length === 0) {
-        $(`
-          <h3>Sorry, no results.</h3>
-        `).appendTo(".book-result");
-      } else {
-        var randomBook = bookWorks[Math.floor(Math.random() * bookWorks.length)];
-        var subjectBookTitle = randomBook.title;
-        console.log(subjectBookTitle);
-        var coverImg = randomBook.cover_id
-        var bookID = randomBook.cover_edition_key
-        $(` 
-        <a href="https://openlibrary.org/books/${bookID}" target="_blank">
-        <img src="http://covers.openlibrary.org/b/id/${coverImg}-M.jpg">
-        <h3 class="card-title">${subjectBookTitle}</h3>
-        </a>
-        
-        <p>Cover image lovingly delivered by <a href="https://openlibrary.org/">OpenLibrary.org</a></p>
-        `).appendTo(".book-result");
+    // Save user search term and comment
+    $("#save-button").on("click", function (event) {
+      event.preventDefault();
+
+      var userSavedTerm = $("#user-search-term").html();
+      var userSavedComment = $("#user-comment").val().trim();
+      console.log(userSavedTerm);
+      console.log(userSavedComment);
+
+      var newUserSave = {
+        searchTerm: userSavedTerm,
+        comment: userSavedComment
       };
-    }); //end of Open Library
 
+      database.ref().push(newUserSave);
+    });
+
+    // Add previous comments to Save Modal
+    database.ref().on("child_added", function (childSnapshot) {
+      console.log(childSnapshot.val());
+      var snap = childSnapshot.val();
+
+      var prevSearchTerm = snap.searchTerm;
+      var prevComment = snap.comment;
+
+      console.log(prevSearchTerm);
+      console.log(prevComment);
+
+      $(`
+    <tr>
+      <td>${prevSearchTerm}</td>
+      <td>${prevComment}</td>
+    </tr>  
+  `).prependTo('#past-searches');
+
+    });
     //Harvard Art Museum
     // Find all of the objects that are paintings and have the "search term" in the title
     var artSearchTerm = $('#compilation-input').val();
@@ -153,12 +152,12 @@ document.addEventListener('DOMContentLoaded', function () {
       var harvardRecord = data.records;
       var randomHarvardRecord = harvardRecord[Math.floor(Math.random() * harvardRecord.length)];
       var randomHarvardImage = randomHarvardRecord.images[0].baseimageurl;
-        if (randomHarvardImage === 'undefined') {
-          $.getJSON();
-        } else {
-          var randomHarvardImageTitle = randomHarvardRecord.title;
-          var randomHarvardLinkBack = randomHarvardRecord.url;
-          var harvardArtResult = $(`
+      if (randomHarvardImage === 'undefined') {
+        $.getJSON();
+      } else {
+        var randomHarvardImageTitle = randomHarvardRecord.title;
+        var randomHarvardLinkBack = randomHarvardRecord.url;
+        var harvardArtResult = $(`
                <img src="${randomHarvardImage}" class="img-fluid harvard-result" alt="Static Image">
                 <h3 class="card-title">Your Inspiring Image</h3>
                 <h5><div>Title: ${randomHarvardImageTitle}</div>
@@ -166,10 +165,13 @@ document.addEventListener('DOMContentLoaded', function () {
                     </a>
                 </h5>
                 <p>Artwork gracefully delivered by <a href="https://www.harvardartmuseums.org/">harvardartmuseums.org</a></p>
-            `)
-          harvardArtResult.appendTo(".harvard-result");
-      }
+            `);
+        harvardArtResult.appendTo(".harvard-result");
+      };
     }); //end of Harvard Art Museum
+
+    //Start of Oxford Dictionary
+    
 
   }); //end of search compilation onClick function
 }); //end of eventListener for DOM loading
