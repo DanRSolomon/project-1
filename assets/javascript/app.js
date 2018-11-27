952e40c03c13f5a4d339c9d4b22b5cfb0f7990f4
Hail Mary <41487770+alejosjen@users.noreply.github.com>
2018-11-26 19:55:38 -0700

    harvard updates with person and sculpture search, need refinement on if statement for person and sculpture

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function () {
   }
   //Clear function
   clear();
-  
+
   //Hide compilation search areas
   $(".card-deck").toggle(false);
   //Search compilations button with onClick function
@@ -45,15 +45,6 @@ document.addEventListener('DOMContentLoaded', function () {
     var userSavedTerm = $("#compilation-input").val();
     $("#user-search-term").html(userSavedTerm);
 
-    //Robohash.org
-    var searchTerm = $("#compilation-input").val();
-    var url = "https://robohash.org/${searchTerm}.png";
-    var robotResult = $(`
-        <img src="${url}" class="img-fluid robot-result" alt="Robot Image">
-        <h3 class="card-title">Your Robot</h3>
-        <p>Robots lovingly delivered by <a href="https://robohash.org/">Robohash.org</a></p>
-    `)
-    robotResult.appendTo(".robot-result");
 
     //OpenLibrary.org
     var userSubjectSearch = $("#compilation-input").val();
@@ -91,23 +82,24 @@ document.addEventListener('DOMContentLoaded', function () {
     // Find all of the objects that are paintings and have the "search term" in the title
     var artSearchTerm = $('#compilation-input').val();
     var apiEndpointBaseURL = "https://api.harvardartmuseums.org/object";
+    var apiKey = "902637e0-edfe-11e8-9463-0b5d77676a26";
     var queryString = $.param({
-      apikey: "902637e0-edfe-11e8-9463-0b5d77676a26",
+      apikey: apiKey,
       title: artSearchTerm,
       classification: "Paintings"
     });
 
     $.getJSON(apiEndpointBaseURL + "?" + queryString, function (data) {
-      console.log(data);
-      var harvardRecord = data.records;
-      var randomHarvardRecord = harvardRecord[Math.floor(Math.random() * harvardRecord.length)];
-      var randomHarvardImage = randomHarvardRecord.images[0].baseimageurl;
-        if (randomHarvardImage === 'undefined') {
-          $.getJSON();
-        } else {
-          var randomHarvardImageTitle = randomHarvardRecord.title;
-          var randomHarvardLinkBack = randomHarvardRecord.url;
-          var harvardArtResult = $(`
+      console.log(data);  
+        var harvardRecord = data.records;
+        var randomHarvardRecord = harvardRecord[Math.floor(Math.random() * harvardRecord.length)];
+        var randomHarvardImage = randomHarvardRecord.images[0].baseimageurl;
+      if (randomHarvardImage === 'undefined' || randomHarvardImage === 0) {
+       $.getJSON();
+      } else {
+        var randomHarvardImageTitle = randomHarvardRecord.title;
+        var randomHarvardLinkBack = randomHarvardRecord.url;
+        var harvardArtResult = $(`
                <img src="${randomHarvardImage}" class="img-fluid harvard-result" alt="Static Image">
                 <h3 class="card-title">Your Inspiring Image</h3>
                 <h5><div>Title: ${randomHarvardImageTitle}</div>
@@ -116,9 +108,81 @@ document.addEventListener('DOMContentLoaded', function () {
                 </h5>
                 <p>Artwork gracefully delivered by <a href="https://www.harvardartmuseums.org/">harvardartmuseums.org</a></p>
             `)
-          harvardArtResult.appendTo(".harvard-result");
+        harvardArtResult.appendTo(".harvard-art-result");
       }
-    }); //end of Harvard Art Museum
+    });
+    // Person search within objects
+    var personQueryString = $.param({
+      apikey: apiKey,
+      person: artSearchTerm,
+    });
+    $.getJSON(apiEndpointBaseURL + "?" + personQueryString, function (data) {
+      console.log(data);
+      var harvardRecord = data.records;
+      var randomHarvardRecord = harvardRecord[Math.floor(Math.random() * harvardRecord.length)];
+      var personRecord = randomHarvardRecord.people[0];
+      if (personRecord === 'undefined' || personRecord === 0) {
+            //Robohash.org
+            var artSearchTerm = $("#compilation-input").val();
+            var url = "https://robohash.org/${artSearchTerm}.png";
+            var robotResult = $(`
+            <img src="${url}" class="img-fluid robot-result" alt="Robot Image">
+            <h3 class="card-title">Your Robot</h3>
+            <h5>Sorry, no person has that name.</h5>
+            <p>Robots lovingly delivered by <a href="https://robohash.org/">Robohash.org</a></p>
+        `)
+        robotResult.appendTo(".harvard-person-result");
+      } else {
+        var personName = personRecord.displayname;
+        var personBirthplace = personRecord.birthplace;
+        var personCulture = personRecord.culture;
+        var personRole = personRecord.role;
+        var workURL = randomHarvardRecord.url;
+        var harvardPersonResult = $(`
+       <h3 class="card-title">Your Inspiring Person</h3>
+       <h5><div>Name: ${personName}</div>
+          <div>Birthplace: ${personBirthplace}</div>
+          <div>Culture: ${personCulture}</div>
+          <div>Role: ${personRole}</div>
+           <a href="${workURL}">Find out more about this person here.
+           </a>
+       </h5>
+       <p>Person highlight brought to you by <a href="https://www.harvardartmuseums.org/">harvardartmuseums.org</a></p>
+   `)
+        harvardPersonResult.appendTo(".harvard-person-result");
+      }
+    }); //end Person search
+    //Sculpture search within objects
+    var sculptureQueryString = $.param({
+      apikey: apiKey,
+      title: artSearchTerm,
+      classification: "Sculpture",
+    })
+    $.getJSON(apiEndpointBaseURL + "?" + sculptureQueryString, function (data) {
+      console.log(data);
+      var harvardRecord = data.records;
+      var randomHarvardRecord = harvardRecord[Math.floor(Math.random() * harvardRecord.length)];
+      var sculptureImage = randomHarvardRecord.images[0].baseimageurl;
+      if (sculptureImage === 'undefined' || sculptureImage === 0) {
+        $.getJSON();
+      } else {
+        var sculptureCulture = randomHarvardRecord.culture;
+        var sculptureTitle = randomHarvardRecord.title;
+        var sculptureURL = randomHarvardRecord.url;
+        var harvardSculptureResult = $(`
+      <img src="${sculptureURL}" class="img-fluid harvard-result" alt="Static Image">
+      <h3 class="card-title">Your Inspiring Person</h3>
+      <h5><div>Title: ${sculptureTitle}</div>
+         <div>Culture: ${sculptureCulture}</div>
+          <a href="${sculptureURL}">Find out more about this sculpture here.
+          </a>
+      </h5>
+      <p>3D-ness brought to you by <a href="https://www.harvardartmuseums.org/">harvardartmuseums.org</a></p>
+  `)
+        harvardSculptureResult.appendTo(".harvard-sculpture-result");
+      }
+    }); //end Sculpture search
+    //end of Harvard Art Museum
 
   }); //end of search compilation onClick function
 }); //end of eventListener for DOM loading
